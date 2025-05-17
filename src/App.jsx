489f8ffdbdbfdcb64fa8d9fcb9d066585445750f
Wah-Ishan <wahasinduishan@gmail.com>
2025-05-17 17:54:48 +0530

    ✅compeled

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,6 @@ function App() {
     <>
      <main>
     <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_9000px_20px_#FFFFFF] - rotate-[30deg] -z-10"></div>
-
-      <Header />
-      <Home />
      </main>
     </>
   )
